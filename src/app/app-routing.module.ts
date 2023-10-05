1f13c34fdd72bcdc09a07c0d255dfd3172432dab
Shailesh.Maurya <shailesh4.maurya@ril.com>
2023-10-05 17:16:49 +0530

    v1.4.0 __adminComponentAdded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { NotFoundComponent } from './commons/not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminComponent},
   { path: 'menu', component: MenuComponent },
-  { path: '', redirectTo: '/home' , pathMatch:'full'},
+  { path: '', redirectTo: '/admin' , pathMatch:'full'},
   { path: '**', component: NotFoundComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,34 @@ import { MenuComponent } from './menu/menu.component';
 import { MenuModule } from './menu/menu.module';
 import { NotFoundComponent } from './commons/not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
+import { AdminModule } from './admin/admin.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './commons/header/header.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MenuModule
+    AdminModule,
+    MenuModule,
+    MatSidenavModule,
+    NgIf, 
+    MatButtonModule,
+    MatToolbarModule, 
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
